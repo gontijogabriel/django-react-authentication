290502afe0303494f1809d3892b926b1d0781410
Gabriel Gontijo <gontijogabr@gmail.com>
2023-10-05 08:06:10 -0300

    upd front

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import GlobalStyles from "./Styles/globalStyles";
+
+
+function App() {
+  return (
+    <div>
+      <GlobalStyles />
+      Olá Mundo!
+    </div>
+
+    );
+}
+export default App
